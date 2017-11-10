4ca960e1e51f5ce136a5c58dca2486ec060eaf62
kadenoldham <shmedly24@gmail.com>
2017-11-10 14:26:22 -0700

    start Contact Folder

---
+++ AR Paper Toss/Kaden's folders/Contact/Contact.swift
@@ -0,0 +1,36 @@
+//
+//  Contact.swift
+//  AR Paper Toss
+//
+//  Created by Kaden Oldham on 11/9/17.
+//  Copyright © 2017 Jeremy Reynolds. All rights reserved.
+//
+
+//import ARKit
+//
+//enum BitTaskCategory: Int {
+//    case paper = 1
+//    case bucket = 2
+//
+//}
+//
+//class Contact: SCNPhysicsContactDelegate {
+//
+//    func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
+//        let nodeA = contact.nodeA
+//        let NodeB = contact.nodeB
+//
+//    }
+//
+//    func whatsWhere(nodeA: SCNNode, nodeB: SCNNode) {
+//
+//        var touched = false
+//        if nodeA.physicsBody?.categoryBitMask ==
+//    }
+//
+//}
+
+
+
+
+
---
+++ AR Paper Toss/Kaden's folders/Models/score.swift
@@ -0,0 +1,13 @@
+//
+//  score.swift
+//  AR Paper Toss
+//
+//  Created by Kaden Oldham on 11/9/17.
+//  Copyright © 2017 Jeremy Reynolds. All rights reserved.
+//
+
+import Foundation
+
+struct Score {
+    var score: Int
+}
