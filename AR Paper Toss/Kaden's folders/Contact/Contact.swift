d69ed7f43fbb06dceaff5008559471f4e587946f
kadenoldham <shmedly24@gmail.com>
2017-11-13 16:14:08 -0700

    merged the contact file with the viewController file

--- AR Paper Toss/Kaden's folders/Contact/Contact.swift
+++ AR Paper Toss/Kaden's folders/Contact/Contact.swift
@@ -8,37 +8,37 @@
 
 import ARKit
 
-enum BitTaskCategory: Int {
-    case paper = 2
-    case cylinder = 3
-    case tube = 4
+//enum BitTaskCategory: Int {
+//    case paper = 2
+//    case cylinder = 3
+//    case tube = 4
+//
+//}
 
-}
 
+//class Contact: UIViewController, SCNPhysicsContactDelegate {
 
-class Contact: UIViewController, SCNPhysicsContactDelegate {
     
-    
-    
-    
-    static let shared = Contact()
-    
-    var cylinder: SCNNode?
-    var tube: SCNNode?
-    
-    
-     func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
-        let nodeA = contact.nodeA
-        let NodeB = contact.nodeB
+//
+//
+//    static let shared = Contact()
+//
+//    var cylinder: SCNNode?
+//    var tube: SCNNode?
+//
+//
+//     func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
+//        let nodeA = contact.nodeA
+//        let NodeB = contact.nodeB
 //        missedIt(nodeA: nodeA, nodeB: NodeB)
-        madeIt(nodeA: nodeA, nodeB: NodeB)
-
-
-
-
-    }
-    
-    
+//        madeIt(nodeA: nodeA, nodeB: NodeB)
+//
+//
+//
+//
+//    }
+//
+//
 
 //     func missedIt(nodeA: SCNNode, nodeB: SCNNode) {
 //
@@ -55,23 +55,23 @@ class Contact: UIViewController, SCNPhysicsContactDelegate {
 //        }
 //
 //    }
-    
-    func madeIt(nodeA: SCNNode, nodeB: SCNNode) {
-        var inBasket = false
-        
-        if nodeA.physicsBody?.categoryBitMask == BitTaskCategory.cylinder.rawValue {
-            self.cylinder = nodeA
-            inBasket = true
-        } else if nodeB.physicsBody?.categoryBitMask == BitTaskCategory.cylinder.rawValue {
-            self.cylinder = nodeB
-            inBasket = true
-        }
-        if inBasket == true {
-            ScoreController.shared.addScore()
-        }
-    }
-
-}
+//
+//    func madeIt(nodeA: SCNNode, nodeB: SCNNode) {
+//        var inBasket = false
+//
+//        if nodeA.physicsBody?.categoryBitMask == BitTaskCategory.cylinder.rawValue {
+//            self.cylinder = nodeA
+//            inBasket = true
+//        } else if nodeB.physicsBody?.categoryBitMask == BitTaskCategory.cylinder.rawValue {
+//            self.cylinder = nodeB
+//            inBasket = true
+//        }
+//        if inBasket == true {
+//            ScoreController.shared.addScore()
+//        }
+//    }
+//
+//}
 
 
 
