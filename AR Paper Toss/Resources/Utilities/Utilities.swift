46f0650392a3d88b4e64a0bf6cb6f0802aa67433
SamuelGarff <31305119+SamuelGarff@users.noreply.github.com>
2017-11-16 13:51:27 -0700

    Fan W/ Wind

--- AR Paper Toss/Resources/Utilities/Utilities.swift
+++ AR Paper Toss/Resources/Utilities/Utilities.swift
@@ -1,9 +1,37 @@
-//
 //  Utilities.swift
-//  AR Paper Toss
+//  PaperTossARDay1
 //
-//  Created by Sam Bryson on 11/16/17.
-//  Copyright © 2017 Jeremy Reynolds. All rights reserved.
+//  Created by Sam Bryson on 11/6/17.
+//  Copyright © 2017 SamuelGBryson. All rights reserved.
 //
 
 import Foundation
+import ARKit
+
+// MARK: - float4x4 extensions
+
+extension float4x4 {
+    /**
+     Treats matrix as a (right-hand column-major convention) transform matrix
+     and factors out the translation component of the transform.
+     */
+    var translation: float3 {
+        let translation = columns.3
+        return float3(translation.x, translation.y, translation.z)
+    }
+}
+
+// MARK: - CGPoint extensions
+
+extension CGPoint {
+    /// Extracts the screen space point from a vector returned by SCNView.projectPoint(_:).
+    init(_ vector: SCNVector3) {
+        x = CGFloat(vector.x)
+        y = CGFloat(vector.y)
+    }
+    
+    /// Returns the length of a point when considered as a vector. (Used with gesture recognizers.)
+    var length: CGFloat {
+        return sqrt(x * x + y * y)
+    }
+}
